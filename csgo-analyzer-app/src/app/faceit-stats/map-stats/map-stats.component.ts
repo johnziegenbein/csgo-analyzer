d356de1f07d18d562ef67e134ab8cd4452680f43
johnziegenbein <johnziegenbein1993@gmail.com>
2019-06-30 16:51:06 +0200

    Make map stats togglable

--- csgo-analyzer-app/src/app/faceit-stats/map-stats/map-stats.component.ts
+++ csgo-analyzer-app/src/app/faceit-stats/map-stats/map-stats.component.ts
@@ -8,10 +8,14 @@ import {Component, Input, OnInit} from '@angular/core';
 export class MapStatsComponent implements OnInit {
 
   @Input() stats;
+  showMaps = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleShowMaps() {
+    this.showMaps = !this.showMaps;
+  }
 }

5259bf35306ec3f3065aa6949cabe1d83a4b5273
johnziegenbein <johnziegenbein1993@gmail.com>
2020-03-28 15:55:58 +0100

    Major refactor on ParseDemo.js
    Also fixed several minor bugs such as NPEs

--- csgo-analyzer-app/src/assets/js/ParseDemo.js
+++ csgo-analyzer-app/src/assets/js/ParseDemo.js
@@ -1,113 +1,134 @@
 const demofile = require('demofile');
 const fs = require('fs');
 
-const roundIntervalStartTicks = 4 * 128
-const roundIntervalStopTicks = 16 * 128
-
 const filename = process.argv[2];
 
-var time = 0;
-var lastroundtime = 0;
-var gamestart = 0;
-var round = -3;
+const T = 2;
+const CT = 3;
 
+const DELIMITER = ",";
+
+let lastRoundTime = 0;
+let round = -3;
 
 fs.readFile(filename, function (err, buffer) {
-    let demoFile = new demofile.DemoFile();
-    let roundstart = false;
-    let freeze_end = false;
+  let demoFile = new demofile.DemoFile();
 
-    demoFile.gameEvents.on('round_start', e => {
-        roundstart = true;
-    round++;
-    if (round === 1) {
+  onRoundStart(demoFile);
+  onRoundFreezeEnd(demoFile);
+  onPlayerDeath(demoFile);
+  onRoundEnd(demoFile);
 
-        console.log("Map," + demoFile.header.mapName);
-
-
-        teams = demoFile.entities.teams;
-        tPlayers = teams[2].members;
-        ctPlayers = teams[3].members;
-
-
-        console.log("Team," + teams[2].clanName + "," + tPlayers[0].name + "," + tPlayers[1].name + "," + tPlayers[2].name + "," + tPlayers[3].name + "," + tPlayers[4].name + ",T");
-        console.log("Team," + teams[3].clanName + "," + ctPlayers[0].name + "," + ctPlayers[1].name + "," + ctPlayers[2].name + "," + ctPlayers[3].name + "," + ctPlayers[4].name + ",CT");
-
-        //console.log('Round,Killer,Victim,Assist,KillerTeam,VictimTeam,Weapon,Headshot,RoundTime,Killerposition,Killeryaw,Victimposition,Victimyaw,KillerStartEQ,KillerFreezeEQ,VictimStartEQ,VictimFreezeEQ')
-
-    }
-
-})
-    ;
-
-    demoFile.gameEvents.on('round_freeze_end', e => {
-        if(roundstart) {
-            freeze_end = true;
-        }
-
-        roundStartTick = demoFile.currentTick;
-    lastroundtime = demoFile.currentTime;
-
-})
-    ;
-
-    demoFile.gameEvents.on('player_death', e => {
-        let victim = demoFile.entities.getByUserId(e.userid);
-    let victimName = victim ? victim.name : '-';
-
-
-    let attacker = demoFile.entities.getByUserId(e.attacker);
-    let attackerName = attacker ? attacker.name : '-';
-
-
-    let headshotText = e.headshot ? 'Yes' : 'No';
-    let time = demoFile.currentTime - lastroundtime;
-
-    //console.log(victim.armor);
-
-    let victimTeam = victim.teamNumber;
-    if (victimTeam === 2) {
-        victimTeam = 'T'
-    } else {
-        victimTeam = 'CT'
-    }
-
-    let attackerTeam = attacker.teamNumber;
-    if (attackerTeam === 2) {
-        attackerTeam = 'T'
-    } else {
-        attackerTeam = 'CT'
-    }
-
-    let assister = demoFile.entities.getByUserId(e.assister);
-    let assisterName = assister ? assister.name : '-';
-
-    if (round >= 1) {
-        //console.log(`${round} ${attackerName} [${e.weapon}${headshotText}] ${victimName}`);
-        console.log("%s,%d,%s,%s,%s,%s,%s,%s,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f",
-            "Kill",round, attackerName, victimName, assisterName, attackerTeam,
-            victimTeam, e.weapon, headshotText, time.toFixed(2), attacker.position.x.toFixed(2),
-            attacker.eyeAngles.yaw.toFixed(2), victim.position.x.toFixed(2), victim.eyeAngles.yaw.toFixed(2), attacker.roundStartEquipmentValue.toFixed(2), attacker.freezeTimeEndEquipmentValue.toFixed(2),
-            victim.roundStartEquipmentValue.toFixed(2), attacker.freezeTimeEndEquipmentValue.toFixed(2));
-    }
-})
-    ;
-
-    demoFile.gameEvents.on('round_end', e => {
-        roundstart = true;
-    let roundWinner = e.winner;
-    if (roundWinner === 2) {
-        roundWinner = 'T';
-    } else {
-        roundWinner = 'CT';
-    }
-
-    if (round >= 1) {
-        console.log("RoundEnded," + roundWinner)
-    }
-    //let lastroundtime = demoFile.currentTime-lastroundtime;
-})
-    ;
-
-    demoFile.parse(buffer);
+  demoFile.parse(buffer);
 });
+
+
+
+function onRoundStart(demoFile) {
+  demoFile.gameEvents.on('round_start', () => {
+    round++;
+    writeGameInformationBeforeFirstRound(demoFile);
+  });
+}
+
+function writeGameInformationBeforeFirstRound(demoFile) {
+  if (round === 1) {
+    console.log("Map," + demoFile.header.mapName);
+
+    let teams = demoFile.entities.teams;
+    let tPlayers = teams[T].members;
+    let ctPlayers = teams[CT].members;
+
+    console.log("Team," + teams[T].clanName + "," + tPlayers[0].name + "," + tPlayers[1].name + "," + tPlayers[2].name + "," + tPlayers[3].name + "," + tPlayers[4].name + ",T");
+    console.log("Team," + teams[CT].clanName + "," + ctPlayers[0].name + "," + ctPlayers[1].name + "," + ctPlayers[2].name + "," + ctPlayers[3].name + "," + ctPlayers[4].name + ",CT");
+  }
+}
+
+function onRoundFreezeEnd(demoFile) {
+  demoFile.gameEvents.on('round_freeze_end', () => {
+    lastRoundTime = demoFile.currentTime;
+  });
+}
+
+function onPlayerDeath(demoFile) {
+  demoFile.gameEvents.on('player_death', deathEvent => {
+
+    if (round >= 1) {
+      let victim = demoFile.entities.getByUserId(deathEvent.userid);
+      let attacker = demoFile.entities.getByUserId(deathEvent.attacker);
+      let assister = demoFile.entities.getByUserId(deathEvent.assister);
+
+      let time = (demoFile.currentTime - lastRoundTime).toFixed(2);
+
+      // Kill, Round, Killer, Victim, Assist, KillerTeam, VictimTeam, Weapon, Headshot, RoundTime, Killerposition,
+      // Killeryaw, Victimposition, Victimyaw, KillerStartEQ, KillerFreezeEQ, VictimStartEQ, VictimFreezeEQ
+      console.log(buildKillinformationString(deathEvent, victim, attacker, assister, time));
+    }
+  });
+}
+
+function buildKillinformationString(deathEvent, victim, attacker, assister, time) {
+  let stringBuiler = "Kill,";
+  stringBuiler += round + DELIMITER;
+  stringBuiler += getName(attacker) + DELIMITER;
+  stringBuiler += getName(victim) + DELIMITER;
+  stringBuiler += getName(assister) + DELIMITER;
+  stringBuiler += getTeam(attacker) + DELIMITER;
+  stringBuiler += getTeam(victim) + DELIMITER;
+  stringBuiler += deathEvent.weapon + DELIMITER;
+  stringBuiler += getHeadShotText(deathEvent) + DELIMITER;
+  stringBuiler += time + DELIMITER;
+  stringBuiler += getPosition(attacker) + DELIMITER;
+  stringBuiler += getYaw(attacker) + DELIMITER;
+  stringBuiler += getPosition(victim) + DELIMITER;
+  stringBuiler += getYaw(victim) + DELIMITER;
+  stringBuiler += getRoundStartEquipmentValue(attacker) + DELIMITER;
+  stringBuiler += getFreezeTimeEndEquipmentValue(attacker) + DELIMITER;
+  stringBuiler += getRoundStartEquipmentValue(victim) + DELIMITER;
+  stringBuiler += getFreezeTimeEndEquipmentValue(victim);
+  return stringBuiler;
+}
+
+function getTeam(playerEntity) {
+  if (playerEntity != null) {
+    return playerEntity.teamNumber === T ? 'T' : 'CT';
+  }
+  return "-";
+}
+
+function getName(playerEntity) {
+  return playerEntity ? playerEntity.name : '-';
+}
+
+function getHeadShotText(deathEvent) {
+  return deathEvent.headshot ? 'Yes' : 'No';
+}
+
+function getPosition(playerEntity) {
+  return playerEntity != null ? playerEntity.position.x.toFixed(2) : "-";
+}
+
+function getYaw(playerEntity) {
+  if (playerEntity != null) {
+    return playerEntity.eyeAngles.yaw.toFixed(2);
+  } else {
+    return "-";
+  }
+}
+
+function getRoundStartEquipmentValue(playerEntity) {
+  return playerEntity != null ? playerEntity.roundStartEquipmentValue.toFixed(2) : "-";
+}
+
+function getFreezeTimeEndEquipmentValue(playerEntity) {
+  return playerEntity != null ? playerEntity.freezeTimeEndEquipmentValue.toFixed(2) : "-";
+}
+
+function onRoundEnd(demoFile) {
+  demoFile.gameEvents.on('round_end', e => {
+    if (round >= 1) {
+      console.log("round_end," + getTeam(e.winner) + "," + e.reason + "," + e.message)
+    }
+  });
+}
+

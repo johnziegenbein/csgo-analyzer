eed0ab19a0c5b9fba141154c0dc58b08763e8bd0
johnziegenbein <johnziegenbein1993@gmail.com>
2020-07-20 19:59:56 +0200

    Create players in teams

--- csgo-analyzer-app/src/app/demo-analyzer/datastructures/player-data.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/datastructures/player-data.ts
@@ -3,4 +3,8 @@ export class PlayerData {
   kills = 0;
   deaths = 0;
   assists = 0;
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
--- csgo-analyzer-app/src/app/demo-analyzer/eventAnalyzers/team-event-analyzer.service.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/eventAnalyzers/team-event-analyzer.service.ts
@@ -5,8 +5,9 @@ import {CS_SIDE, TeamData} from '../datastructures/team-data';
 import {PlayerData} from '../datastructures/player-data';
 
 
-const TEAM_NAME = 1;
-const TEAM_STARTING_SIDE = 7;
+const TEAM_NAME_INDEX = 1;
+const PLAYER_NAMES_INDEX = [2, 3, 4, 5, 6];
+const TEAM_STARTING_SIDE_INDEX = 7;
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +20,22 @@ export class TeamEventAnalyzer implements EventAnalyzer {
 
   addEventAnalysis(demoData: DemoData, demoEvent: string) {
     const eventList = demoEvent.split(',');
-    console.log(eventList);
 
     const teamData: TeamData = {
-      name: eventList[TEAM_NAME],
-      players: new Map<string, PlayerData>(),
-      startAs: eventList[TEAM_STARTING_SIDE] == CS_SIDE.CT ? CS_SIDE.CT : CS_SIDE.T
+      name: eventList[TEAM_NAME_INDEX],
+      players: this.createListOfPlayersFromEvent(eventList),
+      startAs: eventList[TEAM_STARTING_SIDE_INDEX] === CS_SIDE.CT ?
+        CS_SIDE.CT : CS_SIDE.T
     };
     demoData.teams.set(teamData.name, teamData);
+  }
+
+  private createListOfPlayersFromEvent(eventList: string[]) {
+    const playerList = new Map<string, PlayerData>();
+    for (const playerNameIndex of PLAYER_NAMES_INDEX) {
+      const playerName = eventList[playerNameIndex];
+      playerList.set(playerName, new PlayerData(playerName));
+    }
+    return playerList;
   }
 }

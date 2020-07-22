f18cf08fa0ea5e3e4b885b6a45c069b0ba49c94b
johnziegenbein <johnziegenbein1993@gmail.com>
2020-07-22 23:55:43 +0200

    Added more test cases to root out edge cases
    Found problem that DemoData.getTeamBySide did not count with overtimes

--- csgo-analyzer-app/src/app/demo-analyzer/datastructures/demo-data.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/datastructures/demo-data.ts
@@ -7,6 +7,10 @@ export class DemoData {
   matchData: MatchData = new MatchData();
   teams: TeamData[] = [];
 
+  /**
+   * todo: Fix overtime
+   * @param winningTeamSide
+   */
   getTeamBySide(winningTeamSide: string): TeamData {
     if (this.matchData.rounds < SIDE_SWITCH_ROUND) {
       return this.teams.filter(team =>

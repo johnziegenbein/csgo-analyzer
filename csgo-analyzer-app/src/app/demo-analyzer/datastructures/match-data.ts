55e133db373a31ebe075bf157af016abe582df50
johnziegenbein <johnziegenbein1993@gmail.com>
2020-07-26 19:15:10 +0200

    Refactor map in demoData to enum
    This will make handling of map much easier in the future

--- csgo-analyzer-app/src/app/demo-analyzer/datastructures/match-data.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/datastructures/match-data.ts
@@ -1,4 +1,17 @@
 export class MatchData {
-  map = '';
+  map = Map.UNKNOWN;
   rounds = 0;
+}
+
+export enum Map {
+  MIRAGE = 'de_mirage',
+  INFERNO = 'de_inferno',
+  TRAIN = 'de_train',
+  NUKE = 'de_nuke',
+  OVERPASS = 'de_overpass',
+  DUST2 = 'de_dust2',
+  VERTIGO = 'de_vertigo',
+  CACHE = 'de_cache',
+  COBBLESTONE = 'de_cobblestone',
+  UNKNOWN = ''
 }
--- csgo-analyzer-app/src/app/demo-analyzer/event-parsers/map-event-parser.service.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/event-parsers/map-event-parser.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {EventParser} from './event-parser.service';
 import {DemoData} from '../datastructures/demo-data';
+import {Map} from '../datastructures/match-data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class MapEventParser implements EventParser {
   constructor() { }
 
   parseEventIntoDemoData(demoData: DemoData, eventList: string[]) {
-    demoData.matchData.map = eventList[1];
+    demoData.matchData.map = eventList[1] as Map;
   }
 }

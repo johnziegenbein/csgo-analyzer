b07915017e949b19761fb2c644a43ce01fe35506
johnziegenbein <johnziegenbein1993@gmail.com>
2020-07-29 13:26:55 +0200

    Add map background for match results component

--- csgo-analyzer-app/src/app/demo-analyzer/demo-results/match-results/match-results.component.ts
+++ csgo-analyzer-app/src/app/demo-analyzer/demo-results/match-results/match-results.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DemoData} from '../../datastructures/demo-data';
 import {DemoDataService} from '../../demo-data.service';
+import {Map} from '../../datastructures/match-data';
+
 
 @Component({
   selector: 'app-match-results',
@@ -21,5 +23,30 @@ export class MatchResultsComponent implements OnInit {
 
   toggleShowMatchResults() {
     this.showMatchResults = !this.showMatchResults;
+  }
+
+  getImageUrl(map: string) {
+    switch (map) {
+      case Map.INFERNO:
+        return 'url(\'https://www.hltv.org/img/static/maps/inferno.png\')';
+      case Map.CACHE:
+        return 'url(\'https://www.hltv.org/img/static/maps/cache.png\')';
+      case Map.MIRAGE:
+        return 'url(\'https://www.hltv.org/img/static/maps/mirage.png\')';
+      case Map.DUST2:
+        return 'url(\'https://www.hltv.org/img/static/maps/dust2.png\')';
+      case Map.NUKE:
+        return 'url(\'https://www.hltv.org/img/static/maps/nuke.png\')';
+      case Map.VERTIGO:
+        return 'url(\'https://www.hltv.org/img/static/maps/vertigo.png\')';
+      case Map.TRAIN:
+        return 'url(\'https://www.hltv.org/img/static/maps/train.png\')';
+      case Map.OVERPASS:
+        return 'url(\'https://www.hltv.org/img/static/maps/overpass.png\')';
+      case Map.COBBLESTONE:
+        return 'url(\'https://www.hltv.org/img/static/maps/cobblestone.png\')';
+      default:
+        return '';
+    }
   }
 }
